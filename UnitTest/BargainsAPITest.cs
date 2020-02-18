a48f4ffe5a2746a273338bee06043cc340cb6658
Muhammad Ali <muhammad.ali@emaratech.ae>
2020-02-18 05:02:28 +0400

    restructure folder

--- UnitTest/BargainsAPITest.cs
+++ UnitTest/BargainsAPITest.cs
@@ -42,9 +42,9 @@ namespace UnitTest
             {
                 ContractResolver = new CamelCasePropertyNamesContractResolver(),
                 Formatting = Formatting.Indented
-            }); 
+            });
             var expectedResponsestr=GetExpectedHotel();
-            Assert.True(CompareJsonValues(expectedResponsestr, actualresponsestr));   
+            Assert.True(CompareJsonValues(expectedResponsestr, actualresponsestr)); 
         }
 
         private static bool CompareJsonValues(string actualValue, string expectedValue)
@@ -53,6 +53,7 @@ namespace UnitTest
             var expectedObject = JsonConvert.DeserializeObject<JArray>(expectedValue);
             return JToken.DeepEquals(actualObject, expectedObject);
         }
+
         private Task<HttpResponseMessage> GetHotelFromAPI()
         {
             var content = new StringContent("[{\"hotel\":{\"propertyID\":79732,\"name\":\"JAC Canada (CA$)8314\",\"geoId\":279,\"rating\":3},\"rates\":[{\"rateType\":\"Stay\",\"boardType\":\"No Meals\",\"value\":679.2},{\"rateType\":\"Stay\",\"boardType\":\"Half Board\",\"value\":792.4},{\"rateType\":\"Stay\",\"boardType\":\"Full Board\",\"value\":905.6}]},{\"hotel\":{\"propertyID\":79821,\"name\":\"JAC Canada (CA$)8555\",\"geoId\":279,\"rating\":3},\"rates\":[{\"rateType\":\"PerNight\",\"boardType\":\"No Meals\",\"value\":198.0},{\"rateType\":\"PerNight\",\"boardType\":\"Half Board\",\"value\":231.0},{\"rateType\":\"PerNight\",\"boardType\":\"Full Board\",\"value\":264.0}]}]",
