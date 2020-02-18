488e21ebd1108928a7a44d0a678938170e2de40c
Muhammad Ali <muhammad.ali@emaratech.ae>
2020-02-18 07:59:07 +0400

    few more changes

--- src/BargainsAPI/IBargainAPI.cs
+++ src/BargainsAPI/IBargainAPI.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using HotelDetailResponse;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Serilog;
 
 namespace BargainsAPI
 {
@@ -25,26 +24,38 @@ namespace BargainsAPI
 
         public async Task<List<HotelResponse>> FindBargain(int nights, int destination)
         {
-            using var x = ilogger.BeginScope("Calling Bargain API for nights {0}, and destination {1}", nights, destination);
             var uri = new Uri(httpClient.BaseAddress, $"?destinationId={destination}&nights={nights}&code=aWH1EX7ladA8C/oWJX5nVLoEa4XKz2a64yaWVvzioNYcEo8Le8caJw==");
-            // var result = await httpClient.GetAsync("https://webbedsdevtest.azurewebsites.net/api/findBargain?destinationId=1419&nights=2&code=aWH1EX7ladA8C/oWJX5nVLoEa4XKz2a64yaWVvzioNYcEo8Le8caJw==");
-            var result = await httpClient.GetAsync(uri);
-            result.EnsureSuccessStatusCode();
-            var response = await result.Content.ReadAsStringAsync();
-            var hotelResponseDetail = JsonConvert.DeserializeObject<List<HotelResponse>>(response);
-            var task=hotelResponseDetail.Select(i => GetUpdatedRates(i.Rates,nights));
-            var lists = await Task.WhenAll(task).ConfigureAwait(false);
-            return hotelResponseDetail;
+            using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            using var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            var stream = await response.Content.ReadAsStreamAsync();
+
+            response.EnsureSuccessStatusCode();
+            var hotelResponseDetail = DeserializeJsonFromStream<List<HotelResponse>>(stream);
+            var hotels = new List<HotelResponse>();
+            foreach (var hotel in hotelResponseDetail)
+            {
+                var hotelrate = new List<Rate>();
+                foreach (var rate in hotel.Rates)
+                {
+                    if (string.Equals(rate.RateType, "PerNight", StringComparison.InvariantCultureIgnoreCase))
+                        hotelrate.Add(new Rate(rate.RateType, rate.BoardType, rate.Value * nights));
+                    else
+                        hotelrate.Add(rate);
+                }
+                hotels.Add(new HotelResponse(hotel.Hotel, hotelrate));
+            }
+            return hotels;
         }
 
-        private  Task<List<Rate>> GetUpdatedRates(List<Rate> rate,int nights)
+        private static T DeserializeJsonFromStream<T>(Stream stream)
         {
-            for(int a=0;a<rate.Count;a++)
-            {
-                if (string.Equals(rate[a].RateType, "PerNight", StringComparison.InvariantCultureIgnoreCase))
-                    rate[a] = new Rate(rate[a].RateType, rate[a].BoardType, rate[a].Value * nights);
-            }
-            return Task.FromResult(rate);
+            if (stream?.CanRead != true)
+                return default;
+
+            using var sr = new StreamReader(stream);
+            using var jtr = new JsonTextReader(sr);
+            var js = new JsonSerializer();
+            return js.Deserialize<T>(jtr);
         }
     }
 }
